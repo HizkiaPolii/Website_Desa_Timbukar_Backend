de0b3890b21eb621df64b0075aad901653c7b9d2
Hizkia Polii <110800676+HizkiaPolii@users.noreply.github.com>
2025-11-04 18:55:16 +0800

    change limit

--- src/app.ts
+++ src/app.ts
@@ -36,8 +36,8 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Serve uploads folder as static
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
